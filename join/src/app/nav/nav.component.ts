05fd30707c36853fca85954046e38520c0f4f2db
koschkx@gmail.com <96351407+KoschKX@users.noreply.github.com>
2025-06-20 10:43:16 +0200

    Navbar, Header, Footer

--- join/src/app/nav/nav.component.ts
+++ join/src/app/nav/nav.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
 import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-nav',
-  imports: [FooterComponent],
+  standalone: true,
+  imports: [HeaderComponent,FooterComponent],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
