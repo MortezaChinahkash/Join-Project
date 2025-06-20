1dad077a12384953679ab417afe0ec9d4fc37f98
MortezaChinahkash <sascha.chinahkash@gmail.com>
2025-06-20 10:50:30 +0200

    Update main content layout by adding contacts component and refining router outlet structure

--- join/src/app/main-content/main-content.ts
+++ join/src/app/main-content/main-content.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 import { NavComponent } from "../nav/nav.component";
+import { HeaderComponent } from '../header/header.component';
+import { ContactsComponent } from '../contacts/contacts.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavComponent],
+  imports: [CommonModule, RouterOutlet, NavComponent, ContactsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: '../app.component.scss'
 })
