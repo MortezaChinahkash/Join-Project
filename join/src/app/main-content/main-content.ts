2aae65c9d61e22a424b431598528f105c5e9739a
koschkx@gmail.com <96351407+KoschKX@users.noreply.github.com>
2025-06-20 12:08:50 +0200

    Help, Legal Notice,  Privacy Policy pages

--- join/src/app/main-content/main-content.ts
+++ join/src/app/main-content/main-content.ts
@@ -6,10 +6,13 @@ import { NavComponent } from "../nav/nav.component";
 import { HeaderComponent } from '../header/header.component';
 import { ContactsComponent } from '../contacts/contacts.component';
 
+import { HelpComponent } from '../help/help.component';
+import { LegalNoticeComponent } from '../legal-notice/legal-notice.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavComponent, HeaderComponent,ContactsComponent],
+  imports: [CommonModule, RouterOutlet, NavComponent, HeaderComponent, ContactsComponent, HelpComponent, LegalNoticeComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
