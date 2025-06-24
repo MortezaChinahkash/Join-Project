9cb7c92e727a0d7cabf44cdd21850b7f4240a125
Daniel Grabowski <grabowski.daniel@gmail.com>
2025-06-24 10:26:36 +0200

    summary is now the landing page

--- join/src/app/app.routes.ts
+++ join/src/app/app.routes.ts
@@ -1,8 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule} from '@angular/router';
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content';
+import { SummaryComponent } from './summary/summary.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 export const routes: Routes = [
 
-    { path: '', component: MainContentComponent },
+    {
+    path: '',
+    component: MainContentComponent,
+    children: [
+      { path: '', component: SummaryComponent },
+      { path: 'contacts', component: ContactsComponent }
+    ]
+  }
 
 ];
