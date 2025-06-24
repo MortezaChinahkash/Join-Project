e0f251701505069599c0ae0d23120e25875be1a9
MortezaChinahkash <sascha.chinahkash@gmail.com>
2025-06-24 12:19:29 +0200

    Refactor Legal Notice Component Styles and Structure
    - Updated legal-notice.component.scss to enhance styling with a modern design, including a gradient background, improved layout, and responsive adjustments.
    - Introduced a language selector with styled buttons for better user interaction.
    - Added animations for content sections to improve user experience.
    - Modified legal-notice.component.ts to include language selection functionality.
    - Cleaned up main-content.component.scss by removing unnecessary styles.
    - Fixed HTML structure in nav.component.html for better readability and consistency.

--- join/src/app/header/header.component.ts
+++ join/src/app/header/header.component.ts
@@ -1,7 +1,10 @@
 import { Component, HostListener } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
--- join/src/app/legal-notice/legal-notice.component.ts
+++ join/src/app/legal-notice/legal-notice.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-legal-notice',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
 export class LegalNoticeComponent {
+  selectedLanguage: string = 'de';
 
+  setLanguage(language: string) {
+    this.selectedLanguage = language;
+  }
 }
