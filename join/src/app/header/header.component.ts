dca758e4e30affab3712292925c88a6e1bb7925a
MortezaChinahkash <sascha.chinahkash@gmail.com>
2025-06-24 12:21:07 +0200

    Remove duplicate RouterModule import in HeaderComponent

--- join/src/app/header/header.component.ts
+++ join/src/app/header/header.component.ts
@@ -2,7 +2,6 @@ import { Component, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  imports: [RouterModule],
   selector: 'app-header',
   standalone: true,
   imports: [RouterModule],
