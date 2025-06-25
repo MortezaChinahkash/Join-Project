24b14c9465decc5f81e3ca8e8c22540860f0181d
koschkx@gmail.com <96351407+KoschKX@users.noreply.github.com>
2025-06-25 14:38:05 +0200

    inline SVG Loader

---
+++ join/src/app/inline-svg.directive.ts
@@ -0,0 +1,48 @@
+import { Directive, ElementRef, Input, OnChanges, Renderer2, SimpleChanges } from '@angular/core';
+import { SvgLoaderService } from './svg-loader.service';
+
+@Directive({
+  selector: '[inlineSvg]'
+})
+export class InlineSvgDirective implements OnChanges {
+  @Input('inlineSvg') url!: string;
+
+  constructor(
+    private el: ElementRef,
+    private svgLoader: SvgLoaderService,
+    private renderer: Renderer2
+  ) {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['url'] && this.url) {
+      this.svgLoader.loadSvg(this.url).subscribe(svgText => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(svgText, 'image/svg+xml');
+        const svgElement = doc.documentElement;
+        const parent = this.el.nativeElement.parentNode;
+        if (parent && svgElement) {
+          let ngContentAttr: string | null = null;
+          for (let i = 0; i < this.el.nativeElement.attributes.length; i++) {
+            const attr = this.el.nativeElement.attributes[i];
+            if (attr.name.startsWith('_ngcontent-')) {
+              ngContentAttr = attr.name;
+              svgElement.setAttribute(attr.name, '');
+              break;
+            }
+          }
+          const hostClasses = Array.from(this.el.nativeElement.classList) as string[];
+          hostClasses.forEach(cls => svgElement.classList.add(cls));
+          if (ngContentAttr) {
+            const walker = document.createTreeWalker(svgElement, NodeFilter.SHOW_ELEMENT, null);
+            let node = walker.nextNode();
+            while (node) {
+              (node as Element).setAttribute(ngContentAttr, '');
+              node = walker.nextNode();
+            }
+          }
+          parent.replaceChild(svgElement, this.el.nativeElement);
+        }
+      });
+    }
+  }
+}
--- join/src/app/nav/nav.component.ts
+++ join/src/app/nav/nav.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { FooterComponent } from "../footer/footer.component";
 import { RouterModule } from '@angular/router';
 
+import { InlineSvgDirective } from '../inline-svg.directive';
+
 /**
  * Navigation component that provides the main sidebar navigation
  */
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FooterComponent, RouterModule],
+  imports: [FooterComponent, RouterModule, InlineSvgDirective],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
