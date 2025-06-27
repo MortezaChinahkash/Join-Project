931086599e2713688b9b6fdfb0f45a7e59b9ec53
MortezaChinahkash <sascha.chinahkash@gmail.com>
2025-06-27 09:54:19 +0200

    Implement task form with reactive validation and priority selection

--- join/src/app/board/board.component.ts
+++ join/src/app/board/board.component.ts
@@ -1,12 +1,68 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-board',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss'
 })
 export class BoardComponent {
+  taskForm: FormGroup;
+  showAddTaskOverlay = false;
+  selectedPriority: string = '';
 
+  constructor(private fb: FormBuilder) {
+    this.taskForm = this.fb.group({
+      title: ['', Validators.required],
+      description: [''],
+      dueDate: ['', Validators.required],
+      priority: ['', Validators.required],
+      assignedTo: [''],
+      category: ['', Validators.required]
+    });
+  }
+
+  openAddTaskOverlay() {
+    this.showAddTaskOverlay = true;
+    this.resetForm();
+  }
+
+  closeAddTaskOverlay() {
+    this.showAddTaskOverlay = false;
+    this.resetForm();
+  }
+
+  selectPriority(priority: string) {
+    this.selectedPriority = priority;
+    this.taskForm.patchValue({ priority: priority });
+  }
+
+  resetForm() {
+    this.taskForm.reset();
+    this.selectedPriority = '';
+  }
+
+  onSubmit() {
+    if (this.taskForm.valid) {
+      console.log('Task created:', this.taskForm.value);
+      this.closeAddTaskOverlay();
+    } else {
+      this.markFormGroupTouched();
+    }
+  }
+
+  private markFormGroupTouched() {
+    Object.keys(this.taskForm.controls).forEach(key => {
+      const control = this.taskForm.get(key);
+      control?.markAsTouched();
+    });
+  }
+
+  isFieldInvalid(fieldName: string): boolean {
+    const field = this.taskForm.get(fieldName);
+    return !!(field && field.invalid && field.touched);
+  }
 }
